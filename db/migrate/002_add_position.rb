7cf5fa66960f1c3e4f845c86c58bbfffff0778d1
Ing. Martin Hofer <hofer@eyecatch.at>
2010-10-13 12:12:47 +0200

    initial commit

---
+++ db/migrate/002_add_position.rb
@@ -0,0 +1,11 @@
+class AddPosition < ActiveRecord::Migration
+  def self.up
+    add_column :states, :position_x, :integer
+    add_column :states, :position_y, :integer
+  end
+
+  def self.down
+    remove_column :states, :position_y
+    remove_column :states, :position_y
+  end
+end
