79a0d64992ef70edc7242085fd0533b86a8fbe78
Ing. Martin Hofer <hofer@ns3.eyecatch.at>
2010-12-22 15:59:40 +0100

    address

--- clickable_maps_extension.rb
+++ clickable_maps_extension.rb
@@ -4,18 +4,20 @@ class ClickableMapsExtension < Radiant::Extension
   url "http://www.eyecatch.at"
 
   define_routes do |map|
-    map.all_maps 'all_maps', :controller => 'clickable_maps', :action => 'index_maps'
-    map.maps 'show_map/:id', :controller => 'clickable_maps', :action => 'show_map'
-    map.districts 'districts/:id', :controller => 'clickable_maps', :action => 'index_districts'
-    map.districts_list 'districts_list', :controller => 'clickable_maps', :action => 'districts_list'
+    map.all_maps 'all_maps', :controller => 'districts', :action => 'index_maps'
+    map.maps 'show_map/:id', :controller => 'districts', :action => 'show_map'
+    map.districts 'districts/:id', :controller => 'districts', :action => 'index_districts'
+    map.districts_list 'districts_list', :controller => 'districts', :action => 'districts_list'
     map.namespace :admin do |admin|
-       admin.resources :clickable_maps
+       admin.resources :districts
+       admin.resources :address
      end
   end
 
   def activate
     tab 'Content' do
-       add_item "Bezirke", "/admin/clickable_maps", :after => "Pages", :visibility => [:admin]
+       add_item "Bezirke", "/admin/districts", :after => "Pages", :visibility => [:admin]
+       add_item "Adressen", "/admin/address", :after => "Bezirke", :visibility => [:admin]       
     end
     Page.send :include, PdfTags
   end
